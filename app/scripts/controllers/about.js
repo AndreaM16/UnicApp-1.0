84de9f77e6440876202ec6727dde4001162f2953
AndreaM16 <andrea.medda@live.it>
2015-03-10 08:31:21 +0100

    firstpush

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appdoitApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appdoitApp
+ */
+angular.module('appdoitApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
