79e6a0ed7e1d6d270eead830ac02b3b6afa9ad4f
AndreaM16 <andrea.medda@live.it>
2015-03-20 12:19:53 +0100

    buttonWorks

--- app/scripts/imgrepeat.js
+++
@@ -1,9 +0,0 @@
-var app=angular.module('imgr',[]);
-
-app.controller('',function($scope){
-    $scope.img=[
-        {id:'img1',source:'images/img1.jpg'},
-        {id:'img2',source:'images/img2.jpg'},
-        {id:'img3',source:'images/img3.jpg'}
-    ];
-});
