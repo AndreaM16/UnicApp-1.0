e37ebb2f2a6d453fd20e80def9d07e0a50562831
AndreaM16 <andrea.medda@live.it>
2015-03-20 10:19:18 +0100

    SwiperBulletsFix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,5 +40,3 @@ angular
         redirectTo: '/'
       });
   });
-
-
--- app/scripts/swipers.js
+++ app/scripts/swipers.js
@@ -1,11 +1,14 @@
-
+/* Activated by ng-click on the images put in swiper-container.
+	Instantiating an empty swip*/
 var swip = angular.module('swipers',[]);
 
+/* Since I don't need a controller I leave it by default*/
 angular.module('swipers')
-
   .controller('',[ 
   $(document).ready(function (){
     var swiper = new Swiper('.swiper-container',{
-        direction: 'horizontal'
+        direction: 'horizontal',
+        pagination: '.swiper-pagination',
+        paginationClickable: true 
         })
   })]);
