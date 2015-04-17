84d7f5910e59715965b43b2f0ed9a600fa4406a6
AndreaM16 <andrea.medda@live.it>
2015-04-17 12:46:40 +0200

    makingNewsModulesHtml

--- app/scripts/controllers/additionalPagesControllers/news/myNewsController1.js
+++ app/scripts/controllers/additionalPagesControllers/news/myNewsController1.js
@@ -1,4 +1,20 @@
 myApp.controller('myNewsController1', function($scope){
           
-          
+          $scope.myHeaderText = 'Ultime Notizie';
+
+          $scope.myBackground = 'background-image: url('+'../../images/back.png'+')';
+
+          $scope.new1Background = 'background-image: url('+'../../images/consiglio.jpg'+')';
+
+          $scope.textTile = 'Proseguono Incontri Rettore';
+
+          $scope.littleDate = '4-Marzo 2015';
+
+          $scope.longText = 'Proseguono gli incontri dei candidati alla carica di Rettore dell\’Ateneo \
+          					di Cagliari in vista del primo turno di votazioni, fissato per lunedì 9 marzo. \
+          					L\’Ufficio stampa dell\’Università di Cagliari dà conto in questa pagina di alcuni eventi \
+          				    - in particolare di quelli organizzati dalle varie componenti dell\’Ateneo - \
+          				    della campagna elettorale.';
+
+          $scope.nextNew = 'News Successiva';
   });
--- app/scripts/controllers/additionalPagesControllers/news/myNewsController2.js
+++ app/scripts/controllers/additionalPagesControllers/news/myNewsController2.js
@@ -1,4 +1,31 @@
 myApp.controller('myNewsController2', function($scope){
-          
-          
+                
+          $scope.myHeaderText = 'Ultime Notizie';
+
+          $scope.myBackground = 'background-image: url('+'../../images/back.png'+')';
+
+          $scope.new1Background = 'background-image: url('+'../../images/atzori.png'+')';
+
+          $scope.textTile = 'Atzori vince premio Google';
+
+          $scope.littleDate = '3-Marzo 2015';
+
+          $scope.longText = 'Cagliari, 3 marzo 2015 - È stato assegnato a Maurizio Atzori(nella foto a destra), \
+          					 ricercatore del Dipartimento di Matematica ed Informatica dell\’Università di Cagliari,\
+          					 un prestigioso Google Faculty Research Award, premio destinato a finanziare progetti di \
+          					 ricerca innovativi nell\’ambito dell\’ICT con lo scopo dichiarato di sostenere il lavoro \
+          					 di eccellenti ricercatori delle migliori università del mondo”. E’ la prima volta che il \
+          					 prestigioso riconoscimento – una somma di 50mila dollari - viene assegnato ad un ricercatore sardo. \
+							 Selezionato tra 808 progetti provenienti da 55 nazioni, il progetto \“Exploiting Structured Queries \
+							 in Question Answering and Search\”, presentato in collaborazione da Atzori e da Carlo Zaniolo, \
+							 professore della University of California in Los Angeles (UCLA), è uno dei 122 selezionati tra \
+							 cui soltanto 2 provengono da università italiane (UNICA e POLIMI). In passato, il premio è stato \
+							 conferito a ricercatori di 13 atenei italiani, di cui al sud solo a ricercatori di Napoli e Bari. \
+							 Grazie ai fondi della Regione Sardegna, il Professor Zaniolo è stato visiting professor al Dipartimento \
+							 di Matematica e Informatica nel 2011 e nel 2012. Il progetto prende spunto dalle attività sviluppate \
+							 nell\’ambito del progetto DENIS, coordinato dalla professoressa Nicoletta Dessì e \
+							 finanziato dalla legge 7.';
+
+          $scope.nextNew = 'News Precedente';
+ 
   });
